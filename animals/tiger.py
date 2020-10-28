57a5ff4702e6d55bc20a3e7da56d6bfc3a9248db
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-10-27 19:29:52 -0500

    Created Animal Classes with Properties

---
+++ animals/tiger.py
@@ -0,0 +1,11 @@
+from datetime import date
+from .animal import Animal
+
+class Tiger(Animal):
+  def __init__(self, name, species, shift, food, chip_num):
+    super().__init__(name, species, food, chip_num)
+    self.walking = True
+    self.shift = shift
+
+  def __str__(self):
+    return f"{self.name} is a {self.species}"
